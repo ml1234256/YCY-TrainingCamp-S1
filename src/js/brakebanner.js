5a1e72a5136dc4c44a61e44bd56e0f07bbcdb015
ml1234256 <1064575122@qq.com>
2022-07-28 23:58:14 +0800

    update btn

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -6,12 +6,36 @@ class BrakeBanner{
 			backgroundColor: 0xffff00,
 		})
 		document.querySelector(selector).appendChild(this.app.view)
-
+		
+		this.stage = this.app.stage
+		
 		this.loader = new PIXI.Loader()
 		this.loader.add('btn.png', 'images/btn.png')
+		this.loader.add('btn_circle.png', 'images/btn_circle.png')
 		this.loader.load()
+         
+		this.loader.onComplete.add(() => {
+			show()
+		})
 
-		const sprite = PIXI.Sprite(this.loader.reasources['btn.png'].texture)
-		this.app.stage.addChild(sprite)
+		const show = () => { 
+			const actionButton = new PIXI.Container()
+			this.stage.addChild(actionButton)
+
+            const btn = new PIXI.Sprite(this.loader.resources['btn.png'].texture)
+            const btnCircle = new PIXI.Sprite(this.loader.resources['btn_circle.png'].texture) 		    
+			
+			actionButton.addChild(btn)
+			actionButton.addChild(btnCircle)
+
+			btn.pivot.x = btn.pivot.y = btn.width / 2
+			btnCircle.pivot.x = btnCircle.pivot.y = btnCircle.width / 2
+            actionButton.x = actionButton.y = 400
+			
+			btnCircle.scale.x = btnCircle.scale.y = 0.8
+			gsap.to(btnCircle.scale, { duration: 1, x: 1.3, y: 1.3, repeat: -1 })
+			gsap.to(btnCircle, {duration: 1, alpha: 0, repeat: -1})
+		}
+		
 	}
 }
