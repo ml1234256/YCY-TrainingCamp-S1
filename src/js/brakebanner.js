1e7023fc6a30f941c49222922fb5cc2a78f6d89f
ml1234256 <1064575122@qq.com>
2022-07-30 19:48:51 +0800

    添加粒子特效

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -46,10 +46,12 @@ class BrakeBanner{
 
 			actionButton.on('mousedown', () => {
 				gsap.to(brakeLevelImage, {duration:.3, rotation: Math.PI/180*-30})
+			    parse()
 			})
 
 			actionButton.on('mouseup', () => {
 				gsap.to(brakeLevelImage, {duration:.3, rotation: 0})
+			    start()
 			})
             
 
@@ -60,15 +62,84 @@ class BrakeBanner{
 			}
 			window.addEventListener('resize', resize)
 			resize()
+           
+            // 创建粒子
+			const particleContainer = new PIXI.Container()
+			this.stage.addChild(particleContainer)
+
+			particleContainer.pivot.x = window.innerWidth / 2
+			particleContainer.pivot.y = window.innerHeight / 2
+			particleContainer.x = window.innerWidth / 2
+			particleContainer.y = window.innerHeight / 2
+			
+
+			particleContainer.rotation = 35 * Math.PI /180
+
+			const particles = []
+			const colors = [0xf1cf54, 0xb5cea8, 0xf1cf54, 0x818181, 0x000000]
+
+			for (let i = 0; i < 10; i++) {
+				let gr = new PIXI.Graphics()
+				gr.beginFill(colors[Math.floor(Math.random() * colors.length)])
+				gr.drawCircle(0, 0, 6)
+				gr.endFill()
+
+				// 粒子运动初始坐标
+				const pItem = {
+					sx: Math.random() * window.innerWidth,
+					sy: Math.random() * window.innerHeight,
+					gr: gr
+				}
+				
+				gr.x = pItem.sx
+				gr.y = pItem.sy
+
+				particleContainer.addChild(gr)
+				particles.push(pItem)
+			}
+
+			
+			// 让粒子向下运动
+			let speed = 0
+			const loop = () => {
+				speed += 0.5
+				speed = Math.min(speed, 20)
+				for (let i = 0; i < particles.length; i++) {
+					let pItem = particles[i]
+					pItem.gr.y += speed
+					if (speed >= 20) {
+						pItem.gr.scale.x = 0.03
+					    pItem.gr.scale.y = 40
+					}
+					if (pItem.gr.y > window.innerHeight) pItem.gr.y = 0
+				}
+			}
+
+			const start = () => {
+				speed = 0
+				gsap.ticker.add(loop)
+			}
+			const parse = () => {
+				gsap.ticker.remove(loop)
+				for (let i = 0; i < particles.length; i++) {
+					let pItem = particles[i]
+	
+					pItem.gr.scale.x = 1
+					pItem.gr.scale.y = 1
+
+					gsap.to(pItem.gr, {duration: 0.6, x:pItem.sx, y:pItem.sy, ease:'elastic.out'})
+				}
+			}
+			
+            start()
 		}
 		const createActionButton = () => {
 			const actionButton = new PIXI.Container()
-			
+			 
             const btn = new PIXI.Sprite(this.loader.resources['btn.png'].texture)
             const btnCircle = new PIXI.Sprite(this.loader.resources['btn_circle.png'].texture) 		    
 			
-			actionButton.addChild(btn)
-			actionButton.addChild(btnCircle)
+			actionButton.addChild(btn, btnCircle)
 
 			btn.pivot.x = btn.pivot.y = btn.width / 2
 			btnCircle.pivot.x = btnCircle.pivot.y = btnCircle.width / 2
